6dced68b09d7786459a766ff520b8e591a90a54c
Nisanur Genc <81337529+gencnis@users.noreply.github.com>
2021-10-30 22:55:21 -0400

    Add files via upload

---
+++ js/highlightNavBar.js
@@ -0,0 +1,15 @@
+function highlightNav(x) {
+    x.style.color = "#694CA9";
+}
+
+function normalNav(x) {
+    x.style.color = "#30008F";
+}
+
+function highlightMenu(x) {
+    x.style.background = "#E5DFF1";
+}
+
+function normalMenu(x) {
+    x.style.background = "#F4F5F7";
+}
---
+++ js/navigationBar.js
@@ -0,0 +1,26 @@
+function resizeNav() {
+    console.log("Resizing nav bar");
+    const myTitle = document.getElementById("myTitle");
+    const options = document.getElementById("options");
+
+    if (innerWidth > 450 && innerWidth < 1150) {
+        const logoContainerWidth = document.getElementById("logo").clientWidth;
+        let offset = ((innerWidth - myTitle.clientWidth) + (logoContainerWidth / 2)) / 2
+        myTitle.style.left = `${offset}px`;
+    } else {
+        if (myTitle.style.left != "140px") {
+            myTitle.style.left = "140px";
+        }
+    }
+
+    if (innerWidth < 1150) {
+        options.style.display = "none";
+    } else {
+        options.style.display = "inline-block";
+    }
+}
+
+window.addEventListener("load", () => {
+    resizeNav()
+    window.addEventListener("resize", resizeNav);
+})
